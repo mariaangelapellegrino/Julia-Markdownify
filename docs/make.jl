bb737b57876002dd92f9c46af2388d45e6462673
Maria Angela Pellegrino <mariaangelapellegrino94@gmail.com>
2019-04-29 10:24:29 +0200

    update doc generation

--- docs/make.jl
+++ docs/make.jl
@@ -4,12 +4,14 @@ using Markdownify
 makedocs(
     sitename = "Markdownify",
     format = :html,
-    modules = [Markdownify]
+    modules = [Markdownify],
+    makedoc = true
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/mariaangelapellegrino/Markdownify.jl.git",
+    target = "build"
+)
