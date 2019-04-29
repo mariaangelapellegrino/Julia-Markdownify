728107335512288450d2208f8f53d785130a0abe
Maria Angela Pellegrino <mariaangelapellegrino94@gmail.com>
2019-04-29 09:51:48 +0200

    First commit

---
+++ src/Markdownify.jl
@@ -0,0 +1,5 @@
+module Markdownify
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using Test, Markdownify
+
+#setup code
+
+@testset "Test 1" begin
+    @test 4 == 2+2
+    @test_throws DomainError (-1)^0.5
+end;
+
+@testset "Test 2" begin
+    @test 1+1 == 2
+end;
